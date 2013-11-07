1a256c86442718c9602f71e2aba3d205cb7e8f50
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-11-07 03:00:18 -0500

    Homework 4 corrections

--- hw4.js
+++ hw4.js
@@ -24,55 +24,46 @@
 
 // Problem 1
 // Create the object above by setting the properties one by one.
-// athlete.foo = "bar"
-// athlete.method = function () {
-// 
-// }
 
 var athlete1 = {};
-athlete1.name = 'Usain Bolt',
-athlete1.age = 25,
-athlete1.countryOfOrigin = 'Jamaica',
-athlete1.height = '6\'5\"',
-athlete1.medals = ["100m Gold 2008", "200m Gold 2008", "4x100m Relay Gold 2008", "100m Gold 2012", "200m Gold 2012", "4x100m Relay Gold 2012"],
-athlete1.olympian = true;
+athlete1.name = 'Usain Bolt';
+athlete1.age = 25;
+athlete1.countryOfOrigin = 'Jamaica';
+athlete1.height = '6\'5\"';
+athlete1.medals = ["100m Gold 2008", "200m Gold 2008", "4x100m Relay Gold 2008", "100m Gold 2012", "200m Gold 2012", "4x100m Relay Gold 2012"];
+athlete1.isOlympian = true;
 
 athlete1.introduce = function() {
 	var introduction = "";
-	introduction = "'Hello!', my name is " + athlete1.name + " and I am from " + athlete1.countryOfOrigin + ".";
+	introduction = "'Hello!', my name is " + this.name + " and I am from " + this.countryOfOrigin + ".";
 	return introduction;
 };
 
 athlete1.addMedal = function(medal) {
 	medal = medal.toString();
 	athlete1.medals.push(medal);
-	console.log(athlete1.medals);
 };
 
 // Problem 2
 // Create the object using the associate array syntax (i.e. strings within square brackets).
-// athlete['foo'] = "bar"
-// athlete['method'] = function () {
-// 
-// }
+
 var athlete2 = {};
-athlete2['name'] = 'Usain Bolt',
-athlete2['age'] = 25,
-athlete2['countryOfOrigin'] = 'Jamaica',
-athlete2['height'] = '6\'5\"',
-athlete2['medals'] = ["100m Gold 2008", "200m Gold 2008", "4x100m Relay Gold 2008", "100m Gold 2012", "200m Gold 2012", "4x100m Relay Gold 2012"],
-athlete2['olympian'] = true;
+athlete2['name'] = 'Usain Bolt';
+athlete2['age'] = 25;
+athlete2['countryOfOrigin'] = 'Jamaica';
+athlete2['height'] = '6\'5\"';
+athlete2['medals'] = ["100m Gold 2008", "200m Gold 2008", "4x100m Relay Gold 2008", "100m Gold 2012", "200m Gold 2012", "4x100m Relay Gold 2012"];
+athlete2['isOlympian'] = true;
 
 athlete2['introduce'] = function() {
 	var introduction = "";
-	introduction = "'Hello!', my name is " + athlete2.name + " and I am from " + athlete2.countryOfOrigin + ".";
+	introduction = "'Hello!', my name is " + this.name + " and I am from " + this.countryOfOrigin + ".";
 	return introduction;
 };
 
 athlete2['addMedal'] = function(medal) {
 	medal = medal.toString();
 	athlete1.medals.push(medal);
-	console.log(athlete2.medals);
 };
 
 
@@ -86,18 +77,17 @@ var athlete3 = {
 	age: 25,
 	countryOfOrigin: 'Jamaica',
 	medals: 6,
-	olympian: true,
+	isOlympian: true,
 
 	introduce: function() {
 		var introduction;
-		introduction = "'Hello!', my name is " + athlete3.name + " and I am from " + athlete3.countryOfOrigin + ".";
+		introduction = "'Hello!', my name is " + this.name + " and I am from " + this.countryOfOrigin + ".";
 		return introduction;
 	},
 
 	addMedal: function(medal) {
 		medal = medal.toString();
 		athlete1.medals.push(medal);
-		console.log(athlete3.medals);
 	}
 };
 
@@ -106,27 +96,27 @@ var athlete3 = {
 // Create a prototype for athelete. See below for examples:
 // https://github.com/galdamez/ca276-fall2013/blob/master/week8/prototypes.html
 
-function Athlete(name, height, age, countryOfOrigin, medals, olympian) {
-	var name, height, age, countryOfOrigin, medals, olympian;
+function Athlete(name, height, age, countryOfOrigin, medals, isOlympian) {
 	this.name = name.toString();
 	this.height = height.toString();
 	this.age = age;
 	this.countryOfOrigin = countryOfOrigin.toString();
 	this.medals = medals;
-	if (olympian === true || olympian === false)
-		this.olympian = olympian;
 
+	//JG: Always use curly brackets for if/else conditions
+	if (isOlympian === true || isOlympian === false) {
+		this.isOlympian = isOlympian;
+	}
 
 	this.introduce = function() {
 		var introduction;
-		introduction = "'Hello!', my name is " + athlete3.name + " and I am from " + athlete3.countryOfOrigin + ".";
+		introduction = "'Hello!', my name is " + this.name + " and I am from " + this.countryOfOrigin + ".";
 		return introduction;
-	},
+	}
 
 	this.addMedal = function(medal) {
 		medal = medal.toString();
 		athlete1.medals.push(medal);
-		console.log(athlete3.medals);
 	}
 }
 
@@ -152,7 +142,7 @@ a1 = new Athlete("Michael Jordan", "6\'10\"", 50, "USA", ["Basketball Gold 1992"
 // 
 // For example I may want to have a Swimmer type that does the following.
 // 
-// Parameters in order are: name, age, country, height (in meters), medals, is he an Olympian?, preferred brand
+// Parameters in order are: name, age, country, height (in meters), medals, is he an isOlympian?, preferred brand
 // 
 // var s1 = new Swimmer('Michael Phelps', 28, 'USA', 1.93 ['gold', 'gold', 'silver'], true, 'Speedo');
 // 
